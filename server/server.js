7bac581d54db94275e81cb2f8b81f33d42133021
Ernest <ernest2332@gmail.com>
2019-11-15 16:45:00 +0200

    'initial'

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
 const config = require('config');
