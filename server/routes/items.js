6dcfb01efadd26e4aa32f51ac1961857db66423c
Ernest <ernest2332@gmail.com>
2019-11-15 17:35:50 +0200

    some fix

--- server/routes/items.js
+++ server/routes/items.js
@@ -4,8 +4,6 @@ const auth = require('../middleware/auth.js');
 
 const Items = require('../models/items.js');
 
-//Items.find().then(res => console.log(res))
-
 const validateEmail = email => {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
